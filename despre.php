4fb656fd843d152ba5d013664fd842aae7f84125
ionut.gr24 <ionut.gr24@yahoo.com>
2021-04-03 17:36:34 +0300

    added about us page , need to finish design

---
+++ despre.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/about-us.css">
+    <link rel="stylesheet" href="css/variables.css">
+
+    <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+
+
+    <div class="cardWrapper">
+
+        <div class="cardWrapper__card">
+            <div class="cardWrapper__card__defaultState">
+                <div class="cardWrapper__card__imageWrapper">
+                    <img src="img/ionut.png" alt="poza cu ionut">
+                </div>
+                <div class="cardWrapper__card__infoWrapper">
+
+
+                    <h1>Borcila Ionut , <span>20 ani</span> </h1>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est distinctio, fugiat quia aspernatur adipisci iusto! Cum magnam explicabo officia nesciunt natus aperiam, mollitia at quo unde libero facere, corporis quidem!</p>
+
+                </div>
+            </div>
+            <div class="cardWrapper__card__hoverWrapper">
+
+                <div class="cardWrapper__card__hoverWrapper__socials">
+                    <a href="https://www.facebook.com/">
+                        <img src="/img/facebook_social.png">@ionut.gr24</a>
+                    <a href="https://www.instagram.com/">
+                        <img src="/img/instagram_social.png">@ionut.gr24</a>
+                    <a href="https://www.github.com/ionut.gr24">
+                        <img src="/img/github_social.png">@ionut.gr24</a>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+        <div class="cardWrapper__card">
+            <div class="cardWrapper__card__defaultState">
+                <div class="cardWrapper__card__imageWrapper">
+                    <img src="img/ana.png" alt="poza cu ana">
+                </div>
+                <div class="cardWrapper__card__infoWrapper">
+
+
+                    <h1>Danila Ana , <span>20 ani</span> </h1>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est distinctio, fugiat quia aspernatur adipisci iusto! Cum magnam explicabo officia nesciunt natus aperiam, mollitia at quo unde libero facere, corporis quidem!</p>
+
+                </div>
+            </div>
+            <div class="cardWrapper__card__hoverWrapper">
+
+                <div class="cardWrapper__card__hoverWrapper__socials">
+                    <a href="https://www.facebook.com/">
+                        <img src="/img/facebook_social.png">@ana</a>
+                    <a href="https://www.instagram.com/">
+                        <img src="/img/instagram_social.png">@ana</a>
+                    <a href="https://www.github.com/">
+                        <img src="/img/github_social.png">@ana</a>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="cardWrapper__card">
+            <div class="cardWrapper__card__defaultState">
+                <div class="cardWrapper__card__imageWrapper">
+                    <img src="img/alex.png" alt="poza cu alex">
+                </div>
+                <div class="cardWrapper__card__infoWrapper">
+
+
+                    <h1>Harip Alexandru , <span>21 ani</span> </h1>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est distinctio, fugiat quia aspernatur adipisci iusto! Cum magnam explicabo officia nesciunt natus aperiam, mollitia at quo unde libero facere, corporis quidem!</p>
+
+                </div>
+            </div>
+            <div class="cardWrapper__card__hoverWrapper">
+
+                <div class="cardWrapper__card__hoverWrapper__socials">
+                    <a href="https://www.facebook.com/">
+                        <img src="/img/facebook_social.png">@harip.alex</a>
+                    <a href="https://www.instagram.com/">
+                        <img src="/img/instagram_social.png">@harip.alex</a>
+                    <a href="https://www.github.com/">
+                        <img src="/img/github_social.png">@harip.alex</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+
+
+</body>
+
+</html>
