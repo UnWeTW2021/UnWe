0fc61f4e30e0108a5cb1e4f459edc3ac06259cdb
Harip Alexandru <harip.alex@yahoo.com>
2021-04-03 16:07:26 +0300

    [New Code]Added first version for header and footer

---
+++ Components/footer.php
@@ -0,0 +1,17 @@
+<footer class="footerContainer">
+    <div class="footerContainer__siteuri">
+        <p>Site-uri utile</p>
+        <a href="https://iampava.com/web-technologies">Seminar</a>
+        <br>
+        <a href="https://profs.info.uaic.ro/~busaco/teach/courses/web/">Curs</a>
+    </div>
+    <div class="footerContainer__sigla"><img src="resources/SiglaGuvern.png" alt="SiglaGuvern"></div>
+    <div class="footerContainer__socials">
+        <p>Socials</p>
+        <a href="">Facebook</a>
+        <br>
+        <a href="">Twitter</a>
+        <br>
+        <a href="">Instagram</a>
+    </div>
+</footer>
---
+++ Components/header.php
@@ -0,0 +1,13 @@
+<header class="headerContainer">
+<div class="headerContainer__left">
+<a href="../index.php"><img src="resources/SiglaGuvern.png" alt="SiglaGuvern" class="Sigla"></a>
+</div>
+
+<div class="headerContainer__right">
+    <a href="../orase.php">Orașe</a>
+    <a href="../despre.php">Despre noi</a>
+    <a href="../contact.php">Contact</a>
+</div>
+
+
+</header>
