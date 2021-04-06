bb6725924d9c636495b8fd703727a784c43d5949
Harip Alexandru <harip.alex@yahoo.com>
2021-04-06 14:50:32 +0300

    [UPDATE] updated colors on website

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="Mapa/mapdata.js"></script>
     <script type="text/javascript" src="Mapa/countrymap.js"></script>
-    <link rel="stylesheet" href="Components/header-styles.css">
+    <!-- <link rel="stylesheet" href="Components/header-styles.css">
     <link rel="stylesheet" href="Components/footer-styles.css">
+    <script src="Components/header.js" defer></script> -->
+    <?php include('Components/header-include.php'); ?>
     <link rel="stylesheet" href="landingPage.css">
-    <script src="Components/header.js" defer></script>
     <title>LandingPage</title>
 </head>
 <body>
