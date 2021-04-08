fca55d74e1dd79c4310e8a534c87eca76750015a
anamariadanila <79076623+anamariadanila@users.noreply.github.com>
2021-04-08 18:01:24 +0300

    [NEW CODE] Added Orase Page

---
+++ orase.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="judete-lista-style.css">
+    <link rel="stylesheet" href="header-styles.css">
+    <link rel="stylesheet" href="footer-styles.css">
+    <script src="header.js" defer></script>
+    <title>Lista Judetelor</title>
+</head>
+<body >
+<?php include('header.php');?>
+    <section class="judete">
+        <h1>
+            Lista Județelor 
+        </h1>
+        <div class="container flex">
+            <div class="card">
+                <h3>Alba</h3>
+                <a href="Mvc/Public/judet/Alba"></a>
+            </div>
+            <div class="card">
+                <h3>Arad</h3>
+                <a href="Mvc/Public/judet/Arad"></a>
+            </div>
+            <div class="card">
+                <h3>Argeș</h3>
+                <a href="Mvc/Public/judet/Arges"></a>
+            </div>
+            <div class="card">
+                <h3>Bacău</h3>
+                <a href="Mvc/Public/judet/Bacau"></a>
+            </div>
+            <div class="card">
+                <h3>Bihor</h3>
+                <a href="Mvc/Public/judet/Bihor"></a>
+            </div>
+            <div class="card">
+                <h3>Bistrița-Năsăud</h3>
+                <a href="Mvc/Public/judet/Bistrita-Nasaud"></a>
+            </div>
+            <div class="card">
+                <h3>Botoșani</h3>
+                <a href="Mvc/Public/judet/Botosani"></a>
+            </div>
+            <div class="card">
+                <h3>Brăila</h3>
+                <a href="Mvc/Public/judet/Braila"></a>
+            </div>
+            <div class="card">
+                <h3>Brașov</h3>
+                <a href="Mvc/Public/judet/Brasov"></a>
+            </div>
+            <div class="card">
+                <h3>București</h3>
+                <a href="Mvc/Public/judet/Bucharest"></a>
+            </div>
+            <div class="card">
+                <h3>Buzău</h3>
+                <a href="Mvc/Public/judet/Buzau"></a>
+            </div>
+            <div class="card">
+                <h3>Călărași</h3>
+                <a href="Mvc/Public/judet/Calarasi"></a>
+            </div>
+            <div class="card">
+                <h3>Caraș-Severin</h3>
+                <a href="Mvc/Public/judet/Caras-Severin"></a>
+            </div>
+            <div class="card">
+                <h3>Cluj</h3>
+                <a href="Mvc/Public/judet/Cluj"></a>
+            </div>
+            <div class="card">
+                <h3>Constanța</h3>
+                <a href="Mvc/Public/judet/Constanta"></a>
+            </div>
+            <div class="card">
+                <h3>Covasna</h3>
+                <a href="Mvc/Public/judet/Covasna"></a>
+            </div>
+            <div class="card">
+                <h3>Dolj</h3>
+                <a href="Mvc/Public/judet/Dolj"></a>
+            </div>
+            <div class="card">
+                <h3>Dâmbovița</h3>
+                <a href="Mvc/Public/judet/Dambovita"></a>
+            </div>
+            <div class="card">
+                <h3>Galați</h3>
+                <a href="Mvc/Public/judet/Galati"></a>
+            </div>
+            <div class="card">
+                <h3>Giurgiu</h3>
+                <a href="Mvc/Public/judet/Giurgiu"></a>
+            </div>
+            <div class="card">
+                <h3>Gorj</h3>
+                <a href="Mvc/Public/judet/Gorj"></a>
+            </div>
+            <div class="card">
+                <h3>Harghita</h3>
+                <a href="Mvc/Public/judet/Harghita"></a>
+            </div>
+            <div class="card">
+                <h3>Hunedoara</h3>
+                <a href="Mvc/Public/judet/Hunedoara"></a>
+            </div>
+            <div class="card">
+                <h3>Ialomița</h3>
+                <a href="Mvc/Public/judet/Ialomita"></a>
+            </div>
+            <div class="card">
+                <h3>Iași</h3>
+                <a href="Mvc/Public/judet/Iasi"></a>
+            </div>
+            <div class="card">
+                <h3>Ilfov</h3>
+                <a href="Mvc/Public/judet/Ilfov"></a>
+            </div>
+            <div class="card">
+                <h3>Maramureș</h3>
+                <a href="Mvc/Public/judet/Maramures"></a>
+            </div>
+            <div class="card">
+                <h3>Mehedinți</h3>
+                <a href="Mvc/Public/judet/Mehedinti"></a>
+            </div>
+            <div class="card">
+                <h3>Mureș</h3>
+                <a href="Mvc/Public/judet/Mures"></a>
+            </div>
+            <div class="card">
+                <h3>Neamț</h3>
+                <a href="Mvc/Public/judet/Neamt"></a>
+            </div>
+            <div class="card">
+                <h3>Olt</h3>
+                <a href="Mvc/Public/judet/Olt"></a>
+            </div>
+            <div class="card">
+                <h3>Prahova</h3>
+                <a href="Mvc/Public/judet/Prahova"></a>
+            </div>
+            <div class="card">
+                <h3>Sălaj</h3>
+                <a href="Mvc/Public/judet/Salaj"></a>
+            </div>
+            <div class="card">
+                <h3>Satu Mare</h3>
+                <a href="Mvc/Public/judet/Satu-Mare"></a>
+            </div>
+            <div class="card">
+                <h3>Sibiu</h3>
+                <a href="Mvc/Public/judet/Sibiu"></a>
+            </div>
+            <div class="card">
+                <h3>Suceava</h3>
+                <a href="Mvc/Public/judet/Suceava"></a>
+            </div>
+            <div class="card">
+                <h3>Teleorman</h3>
+                <a href="Mvc/Public/judet/Teleorman"></a>
+            </div>
+            <div class="card">
+                <h3>Timiș</h3>
+                <a href="Mvc/Public/judet/Timis"></a>
+            </div>
+            <div class="card">
+                <h3>Tulcea</h3>
+                <a href="Mvc/Public/judet/Tulcea"></a>
+            </div>
+            <div class="card">
+                <h3>Vaslui</h3>
+                <a href="Mvc/Public/judet/Vaslui"></a>
+            </div>
+            <div class="card">
+                <h3>Vrancea</h3>
+                <a href="Mvc/Public/judet/Vrancea"></a>
+            </div>
+            <div class="card">
+                <h3>Vâlcea</h3>
+                <a href="Mvc/Public/judet/Valcea"></a>
+            </div>
+        </div>
+    </section>
+    <?php include('footer.php');?>
+</body>
+</html>
