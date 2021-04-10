a3470eaada3c033590d871a9eb9d88a7f2c35893
ionut.gr24 <ionut.gr24@yahoo.com>
2021-04-10 18:41:29 +0300

    retouches on desgin

--- despre.php
+++ despre.php
@@ -1,20 +1,14 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="ro">
 
 <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Despre</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta property="og:title" content="">
-    <meta property="og:type" content="">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
 
-    <link rel="manifest" href="site.webmanifest">
-    <link rel="apple-touch-icon" href="icon.png">
-    <!-- Place favicon.ico in the root directory -->
+
 
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css">
@@ -25,7 +19,9 @@
 </head>
 
 <body>
-
+<?php
+    # include('Components/header.php');
+    ?>
 
     <div class="cardWrapper">
 
@@ -46,12 +42,12 @@
             <div class="cardWrapper__card__hoverWrapper">
 
                 <div class="cardWrapper__card__hoverWrapper__socials">
-                    <a href="https://www.facebook.com/">
-                        <img src="/img/facebook_social.png">@ionut.gr24</a>
-                    <a href="https://www.instagram.com/">
-                        <img src="/img/instagram_social.png">@ionut.gr24</a>
-                    <a href="https://www.github.com/ionut.gr24">
-                        <img src="/img/github_social.png">@ionut.gr24</a>
+                    <a href="https://www.facebook.com/andreiionut24/">
+                        <img src="img/facebook_social.png">@andreiionut24</a>
+                    <a href="https://www.instagram.com/ionut.gr24/">
+                        <img src="img/instagram_social.png">@ionut.gr24</a>
+                    <a href="https://www.github.com/ionutgr24">
+                        <img src="img/github_social.png">@ionutgr24</a>
                 </div>
 
             </div>
@@ -68,7 +64,7 @@
                 <div class="cardWrapper__card__infoWrapper">
 
 
-                    <h1>Danila Ana , <span>20 ani</span> </h1>
+                    <h1>Danila Ana , <span>21 ani</span> </h1>
 
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est distinctio, fugiat quia aspernatur adipisci iusto! Cum magnam explicabo officia nesciunt natus aperiam, mollitia at quo unde libero facere, corporis quidem!</p>
 
@@ -77,12 +73,12 @@
             <div class="cardWrapper__card__hoverWrapper">
 
                 <div class="cardWrapper__card__hoverWrapper__socials">
-                    <a href="https://www.facebook.com/">
-                        <img src="/img/facebook_social.png">@ana</a>
-                    <a href="https://www.instagram.com/">
-                        <img src="/img/instagram_social.png">@ana</a>
-                    <a href="https://www.github.com/">
-                        <img src="/img/github_social.png">@ana</a>
+                    <a href="https://www.facebook.com/ana.danila.3/">
+                        <img src="img/facebook_social.png">@ana.danila.3</a>
+                    <a href="https://www.instagram.com/ana.danilaa">
+                        <img src="img/instagram_social.png">@ana.danilaa</a>
+                    <a href="https://www.github.com/anamariadanila">
+                        <img src="img/github_social.png">@anamariadanila</a>
                 </div>
 
             </div>
@@ -105,12 +101,12 @@
             <div class="cardWrapper__card__hoverWrapper">
 
                 <div class="cardWrapper__card__hoverWrapper__socials">
-                    <a href="https://www.facebook.com/">
-                        <img src="/img/facebook_social.png">@harip.alex</a>
-                    <a href="https://www.instagram.com/">
-                        <img src="/img/instagram_social.png">@harip.alex</a>
-                    <a href="https://www.github.com/">
-                        <img src="/img/github_social.png">@harip.alex</a>
+                    <a href="https://www.facebook.com/alex.harip">
+                        <img src="img/facebook_social.png">@alex.harip</a>
+                    <a href="https://www.instagram.com/harip.alex/">
+                        <img src="img/instagram_social.png">@harip.alex</a>
+                    <a href="https://github.com/alharip">
+                        <img src="img/github_social.png">@alharip</a>
                 </div>
 
             </div>
@@ -120,11 +116,11 @@
     </div>
 
 
+    <?php
+   # include('Components/footer.php');
+    ?>
 
 
-    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="js/main.js"></script>
 
 
 
