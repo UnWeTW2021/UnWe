7730d8586d9381637ff2403b625e7ccc09bb0cbd
ionutgr24 <ionut.gr24@yahoo.com>
2021-06-06 00:05:55 +0300

    added forms refactor

--- admin.php
+++ admin.php
@@ -9,8 +9,7 @@
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/admin.css">
-    <?php include('Components/header-include.php');?>
-
+    <?php include('Components/header-include.php'); ?>
 
 
 </head>
@@ -33,16 +32,17 @@ include('Components/header.php');
         <p class="error-message">Selecteaza macar un dataset</p>
     </div>
 
-    <div class="admin-form-wrapper">
-        <form id="admin-form" >
+    <div class="admin-form-wrapper" id="admin-form-wrapper">
+
+
+        <form id="grupa-varsta">
             <label for="judet">Judet:</label>
-          <input type="text" name="judet" value="Judet" required>
-            <label for="luna">Luna</label>
+            <input type="text" name="judet" value="Judet" required>
             <div class="grupa-varsta-admin">
                 <label for="sub25">Sub 25 ani:</label>
-                <input type="number" name="sub25" id="sub25" >
+                <input type="number" name="sub25" id="sub25">
                 <label for="_25_29">25-29 ani:</label>
-                <input type="number" name="_25_29" id="25_29" >
+                <input type="number" name="_25_29" id="25_29">
                 <label for="_30_39">30-39 ani:</label>
                 <input type="number" name="_30_39" id="30_39">
                 <label for="_40_49">40-49 ani:</label>
@@ -51,7 +51,19 @@ include('Components/header.php');
                 <input type="number" name="_50_55" id="50_55">
                 <label for="peste55">Peste 55 ani:</label>
                 <input type="number" name="peste55" id="peste55">
+
             </div>
+            <label for="luna">Luna</label>
+            <input type="number" name="luna" value="1" min="1" max="12" required >
+            <label for="luna">An</label>
+            <input type="number" name="an" value="2021" required >
+            <button type="submit" id="grupa-varsta-submit">Submit</button>
+        </form>
+        <form id="medie-rezidenta">
+            <label for="judet">Judet:</label>
+            <input type="text" name="judet" value="Judet" required>
+
+
             <div class="medie-rezidenta-admin">
                 <label for="total_femei">Total femei:</label>
                 <input type="number" name="total_femei" id="total_femei">
@@ -70,22 +82,44 @@ include('Components/header.php');
                 <label for="barbati_rural">barbati rural:</label>
                 <input type="number" name="barbati_rural" id="barbati_rural">
             </div>
-<div class="nivelul-educatiei-admin">
-    <label for="fara_studii">fara studii:</label>
-    <input type="number" name="fara_studii" id="fara_studii">
-    <label for="invatamant_primar">primar:</label>
-    <input type="number" name="invatamant_primar" id="invatamant_primar">
-    <label for="invatamant_gimnazial">gimnazial:</label>
-    <input type="number" name="invatamant_gimnazial" id="invatamant_gimnazial">
-    <label for="invatamant_liceal">liceal:</label>
-    <input type="number" name="invatamant_liceal" id="invatamant_liceal">
-    <label for="invatamant_postliceal">postiliceal:</label>
-    <input type="number" name="invatamant_postliceal" id="invatamant_postliceal">
-    <label for="invatamant_profesional">profesional:</label>
-    <input type="number" name="invatamant_profesional" id="invatamant_profesional">
-    <label for="invatamant_universitar">universitar:</label>
-    <input type="number" name="invatamant_universitar" id="invatamant_universitar">
-</div>
+            <label for="luna">Luna</label>
+            <input type="number" name="luna" value="1" min="1" max="12" required >
+            <label for="luna">An</label>
+            <input type="number" name="an" value="2021" required >
+            <button type="submit" id="medie-rezidenta-submit">Submit</button>
+        </form>
+
+        <form id="nivelul-educatiei">
+            <label for="judet">Judet:</label>
+            <input type="text" name="judet" value="Judet" required>
+
+            <div class="nivelul-educatiei-admin">
+                <label for="fara_studii">fara studii:</label>
+                <input type="number" name="fara_studii" id="fara_studii">
+                <label for="invatamant_primar">primar:</label>
+                <input type="number" name="invatamant_primar" id="invatamant_primar">
+                <label for="invatamant_gimnazial">gimnazial:</label>
+                <input type="number" name="invatamant_gimnazial" id="invatamant_gimnazial">
+                <label for="invatamant_liceal">liceal:</label>
+                <input type="number" name="invatamant_liceal" id="invatamant_liceal">
+                <label for="invatamant_postliceal">postiliceal:</label>
+                <input type="number" name="invatamant_postliceal" id="invatamant_postliceal">
+                <label for="invatamant_profesional">profesional:</label>
+                <input type="number" name="invatamant_profesional" id="invatamant_profesional">
+                <label for="invatamant_universitar">universitar:</label>
+                <input type="number" name="invatamant_universitar" id="invatamant_universitar">
+            </div>
+            <label for="luna">Luna</label>
+            <input type="number" name="luna" value="1" min="1" max="12" required >
+            <label for="luna">An</label>
+            <input type="number" name="an" value="2021" required >
+            <button type="submit" id="niveul-educatiei-submit">Submit</button>
+        </form>
+
+        <form id="rate-somaj">
+            <label for="judet">Judet:</label>
+            <input type="text" name="judet" value="Judet" required>
+
             <div class="rate-somaj-admin">
                 <label for="rata_total_femei">numar total femei:</label>
                 <input type="number" name="rata_total_femei" id="rata_total_femei">
@@ -102,10 +136,10 @@ include('Components/header.php');
                 <label for="rata_somaj_masculina">rata somaj masculina:</label>
                 <input type="number" name="rata_somaj_masculina" id="rata_somaj_masculina">
             </div>
-            <input type="number" name="luna" value="1" min="1" max="12" required id="luna">
+            <input type="number" name="luna" value="1" min="1" max="12" required >
             <label for="luna">An</label>
-            <input type="number" name="an" value="2021" required id="an">
-            <button type="submit"  id="admin-form-submit">Submit</button>
+            <input type="number" name="an" value="2021" required >
+            <button type="submit" id="rate-somaj-submit">Submit</button>
         </form>
     </div>
 
@@ -113,22 +147,22 @@ include('Components/header.php');
 
 
         <section class="table-responsive">
-<!--            <table class="table">-->
-<!--                <thead>-->
-<!--                <tr>-->
-<!--                    <th scope="col">id</th>-->
-<!--                    <th scope="col">Name</th>-->
-<!--                    <th scope="col">Category</th>-->
-<!--                    <th scope="col">Price</th>-->
-<!--                    <th scope="col">Date</th>-->
-<!--                </tr>-->
-<!--                </thead>-->
-<!--                <tbody id="list">-->
-<!---->
-<!---->
-<!---->
-<!--                </tbody>-->
-<!--            </table>-->
+            <!--            <table class="table">-->
+            <!--                <thead>-->
+            <!--                <tr>-->
+            <!--                    <th scope="col">id</th>-->
+            <!--                    <th scope="col">Name</th>-->
+            <!--                    <th scope="col">Category</th>-->
+            <!--                    <th scope="col">Price</th>-->
+            <!--                    <th scope="col">Date</th>-->
+            <!--                </tr>-->
+            <!--                </thead>-->
+            <!--                <tbody id="list">-->
+            <!---->
+            <!---->
+            <!---->
+            <!--                </tbody>-->
+            <!--            </table>-->
         </section>
         <div class="pagination">
             <button class="pagination-button" id="prev">Previous</button>
