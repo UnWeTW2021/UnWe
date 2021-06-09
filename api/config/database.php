8f8ebd0d0ce00fe9bd28b0101577d107aa70a83d
ionutgr24 <ionut.gr24@yahoo.com>
2021-06-09 13:50:06 +0300

    added db conn

--- api/config/database.php
+++ api/config/database.php
@@ -1,39 +1,39 @@
 <?php
 
-    class Database{
-        // specify your own database credentials
-        private $host = "localhost";
-        private $db_name = "unwe";
-        private $username = "root";
-        private $password = "";
-        public $conn;
-  
+class Database{
+    // specify your own database credentials
+    private $host = "eu-cdbr-west-01.cleardb.com";
+    private $db_name = "heroku_5b996654482468a";
+    private $username = "b11327676eab99";
+    private $password = "c5ab036d";
+    public $conn;
+
     // get the database connection
     public function getConnection(){
-  
+
         $this->conn = null;
-  
+
         try{
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
         }
-  
+
         return $this->conn;
     }
-    }
+}
 
 function OpenCon()
 {
 
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
+    $servername = "eu-cdbr-west-01.cleardb.com";
+    $username = "b11327676eab99";
+    $password = "c5ab036d";
 
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=unwe", $username, $password);
+        $conn = new PDO("mysql:host=$servername;dbname=heroku_5b996654482468a", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //   echo "Connected successfully";
